2092e75dccd338aa2188e97d228eb66cf26a03f9
LightDiscord <root@arnaud.sh>
2018-10-16 17:58:08 +0200

    communication

--- index.js
+++ index.js
@@ -2,16 +2,14 @@ console.log(':wave: world!');
 
 const elements = {
     host: document.querySelector('#host'),
+    hostOutput: document.querySelector('#host-output'),
     connect: document.querySelector('#connect'),
     connectInput: document.querySelector('#connect-input'),
+    answer: document.querySelector('#answer'),
+    answerInput: document.querySelector('#answer-input'),
 }
 
 const connection = new RTCPeerConnection();
-
-const gotOffer = (desc) => {
-    connection.setLocalDescription(desc);
-    console.log(desc);
-}
 
 const gotAnswer = (desc) => {
     connection.setRemoteDescription(desc);
@@ -25,14 +23,22 @@ connection.onaddstream = gotRemoteStream;
 
 const createOffer = (connection) => {
     return connection.createOffer().then((description) => {
-        console.log('got description', description, description.toJSON());
+        console.log('got description', description);
 
-        connection.setLocalDescription(description).then(() => {
-            console.log({
-                sdp: connection.localDescription,
-            })
-        })
+        elements.hostOutput.value = JSON.stringify(description);
+
+        return connection.setLocalDescription(description)
     })
+}
+
+const channel = connection.createDataChannel('chat', { negotiated: true, id: 0 });
+
+channel.onopen = event => {
+    channel.send(':wave:');
+}
+
+channel.onmessage = event => {
+    console.log('receive', event)
 }
 
 elements.host.addEventListener('click', () => {
@@ -40,6 +46,20 @@ elements.host.addEventListener('click', () => {
 });
 
 elements.connect.addEventListener('click', () => {
-    // connection.setRemoteDescription(elements.connectInput.value);
-    
+    const description = JSON.parse(elements.connectInput.value);
+
+    console.log(description);
+
+    connection.setRemoteDescription(description)
+        .then(() => connection.createAnswer())
+        .then((description) => {
+            console.log(description)
+            return connection.setLocalDescription(description)
+        })
+})
+
+elements.answer.addEventListener('click', () => {
+    const description = JSON.parse(elements.answerInput.value);
+
+    connection.setRemoteDescription(description);
 })
